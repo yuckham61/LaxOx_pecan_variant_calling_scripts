3586165a14b543b587b48a53d91a816986ad0b64
yuckham61 <duwadi61@gmail.com>
2025-11-08 13:15:21 -0500

    Update check_sample.sh

--- check_sample.sh
+++ check_sample.sh
@@ -7,10 +7,5 @@ sample=$(basename "$file" .txt.trimmed_seqs.txt)
 
 echo $sample
 
-#bwa mem -t 20 data/genomes/C_illinoinensisOaxaca_genomic.fna "$file" > results/sam/${sample}.sam
-
-# Convert sam file to bam file
-#samtools view -@20 -S -b results/sam/$sample.sam > results/bam/$sample.bam
-
 done 
 
